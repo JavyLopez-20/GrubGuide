0f33d6557af68f7bb4eeb239a2a551f5b7ec373e
Javier Lopez <javylopez98@hotmail.com>
2025-02-26 19:28:44 -0600

    updated models

---
+++ server/models/restaurant.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.model({
+    yelpId: { type: String, required: true, unique: true},
+
+    name: { type: String, required: true},
+    cuisine: { type: String, required: true},
+        location: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        zipCode: { type: String, required: true },
+    coordinates: {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    },
+    },
+    rating: { type: Number, required: true },
+    pricing: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+});
+
+module.exports = mongoose.model("restaurant", restaurantSchema);
